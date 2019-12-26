19189794f5210dd24838daec26eec0459a6d4026
Robert Wise <robertwise64@gmail.com>
2019-12-26 15:46:34 -0600

    Basic ChordProgression abstract implementation outline

---
+++ ChoraleGenerator/src/chordProgression/ChordProgression.java
@@ -0,0 +1,31 @@
+package chordProgression;
+
+public abstract class ChordProgression {
+	// Single note key, since implementing classes will designate major/minor
+	private int key;
+	private Chord[] progression;
+	private int length;
+	
+	// Should generate progression based on key and length
+	public ChordProgression(int key, int length) {
+		// Reduce key to min value
+		this.key = key % 12;
+		this.length = length;
+		this.generate();
+	}
+	
+	// Initialize progression from existing chords
+	public ChordProgression(int key, Chord[] progression) {
+		// Reduce key to min value
+		this.key = key % 12;
+		this.progression = progression;
+		this.length = progression.length;
+	}
+	
+	// Should generate a progression based on the key and length
+	abstract void generate();
+	
+	public void play() {
+		// TODO generate chord progression midi
+	}
+}
