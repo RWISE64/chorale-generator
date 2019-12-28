a13ff8efd6d15b36dfacf8b86e1b53f0a135b489
Robert Wise <robertwise64@gmail.com>
2019-12-27 20:31:09 -0600

    Rewrote major progression generator to use Roman numerals

--- ChoraleGenerator/src/chordProgression/ChordProgression.java
+++ ChoraleGenerator/src/chordProgression/ChordProgression.java
@@ -1,8 +1,6 @@
 package chordProgression;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
+import static jm.constants.Chords.*;
 import jm.gui.show.ShowScore;
 import jm.music.data.CPhrase;
 import jm.music.data.Part;
@@ -14,12 +12,12 @@ public abstract class ChordProgression {
 	protected int key;
 	protected int[][] progression;
 	protected int length;
-	protected final int octave = 12;
+	protected final int OCTAVE = 12;
 	
 	// Should generate progression based on key and length
 	public ChordProgression(int key, int length) {
 		// Reduce key to min value
-		this.key = key % octave;
+		this.key = key % OCTAVE;
 		this.length = length;
 		this.generate();
 	}
@@ -27,9 +25,44 @@ public abstract class ChordProgression {
 	// Initialize progression from existing chords
 	public ChordProgression(int key, int[][] progression) {
 		// Reduce key to min value
-		this.key = key % octave;
+		this.key = key % OCTAVE;
 		this.progression = progression;
 		this.length = progression.length;
+	}
+	
+	// Returns int[] of intervals based on Roman numeral cur
+	protected int[] getIntervals(String cur) {
+		String[] parts = cur.split("_");
+		// Major if first char is I or V
+		if (cur.charAt(0) == 'I' || cur.charAt(0) == 'V') {
+			// Only one section => major triad
+			if (parts.length == 1)
+				return MAJOR;
+			else if (parts[1].equals("m7"))
+				return SEVENTH;
+			else 
+				return MAJOR_SEVENTH;
+		}
+		// Otherwise, minor/diminished (i or v)
+		else {
+			// Only one section => minor triad
+			if (parts.length == 1)
+				return MINOR;
+			// Currently ignoring 
+			else if (parts.length == 2) {
+				if (parts[1].equals("d"))
+					return DIMINISHED;
+				else
+					return MINOR_SEVENTH;
+			}
+			// Only dim7 chords have 3 parts so far
+			else {
+				if (parts[2].equals("m7"))
+					return MINOR_SEVENTH_FLAT_FIFTH;
+				else
+					return DIMINISHED_SEVENTH;
+			}
+		}
 	}
 	
 	// Should generate a progression based on the key and length
